70ef52f211a627901c16d02bad502022d90096df
Nathan Hartford <71589945+njhartfo@users.noreply.github.com>
2021-07-17 06:21:01 -0500

    Update window.py

--- file_renamer/ui/window.py
+++ file_renamer/ui/window.py
@@ -1,1 +1,16 @@
+# -*- coding: utf-8 -*-
 
+# Form implementation generated from reading ui file 'window.ui'
+#
+# Created by: PyQt5 UI code generator 5.14.1
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_Window(object):
+  def setupUi(self, Window):
+    Window.setObjectName("Window)
+    Window.resize(720, 480)
+    self.gridLayout = QtWidgets.QGridLayout(Window)
+    self.gridLayout.setObjectname("gridLayout")

113b358e145a682b173c0a25757f48daa65cefad
Nathan Hartford <71589945+njhartfo@users.noreply.github.com>
2021-07-18 06:29:10 -0500

    Update rename.py

--- file_renamer/rename.py
+++ file_renamer/rename.py
@@ -1,1 +1,20 @@
+import time
+from pathlib import Path
+from PyQt5.QtCore import QObject, pyqtSignal
 
+
+class Renamer(QObject):
+  progressed = pyqtSignal(int)
+  renamedFile = pyqtSignal(Path)
+  finished = pyqtSignal() # come back to review, not sure if correct
+  
+  def __init__(self, files, prefix):
+    super().__init__()
+    self._files = files
+    self._prefix = prefix
+  
+  def renameFiles(self):
+    # parse through files
+    # join under parent file
+    # rename
+    # emit

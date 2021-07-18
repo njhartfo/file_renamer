a455da7c78c76a54cd63c20c6a72b5be189890d7
Nathan Hartford <71589945+njhartfo@users.noreply.github.com>
2021-07-18 06:22:49 -0500

    Update app.py

--- file_renamer/app.py
+++ file_renamer/app.py
@@ -1,1 +1,13 @@
+import sys
 
+from PyQt5.QtWidgets import QApplication
+from .views import Window
+
+
+def main():
+  # Create the applicaiton
+  app = QApplication(sys.argv)
+  # Create main window
+  win = Window()
+  win.show()
+  sys.exit(app.exec())

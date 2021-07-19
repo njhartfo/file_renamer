93d77ace32f2d3c8accc9d5050207e388f0d11b2
Nathan Hartford <71589945+njhartfo@users.noreply.github.com>
2021-07-19 05:56:50 -0500

    Update views.py

--- file_renamer/views.py
+++ file_renamer/views.py
@@ -50,17 +50,50 @@ class Window(QWidget, Ui_Window):
 		self.prefixEdit.TextChanged.connect(self._updateStateWhenReady)
 		
 	def _updateStateWhenReady(self):
+		if self.prefixEdit.text():
+			self.renameFilesButton.setEnabled(True)
+		else:
+			self.renameFilesButton.SetEnabled(False)
 		
 	def loadFiles(self):
+		self.dstFileList.clear()
+		if self.dirEdit.text():
+			intiDir = self.dirEdit.text()
+		else:
+			initDir = str(Path.home())
+		files, filter = QFileDialog.getOpenFileNames(
+			self, "Choose Files to Rename", initDir, filter=FILTERS
+		)
+		if len(files) > 0:
+			fileExtension = filter[filter.index("*") : -1]
+			self.extensionLabel.setText(fileExtension)
+			scrDirName = str(Path(files[0]).parent)
+			self.dirEdit.setText(srcDirName)
+			for file in files:
+				self._files.append(Path(file))
+				self.scrFileList.addItem(file)
+			self._fileCount = len(self._files)
+			self._updateStateWhenFilesLoaded()
 		
 	def _updateStateWhenFilesLoaded(self):
+		self.prefixEdit.setEnabled(True)
+		self.prefixEdit.setFocus(True)
 	
 	def renameFiles(self):
+		self._runRenamerThread()
+		self._updateStateWhileRenaming()
 		
 	def _updateStateWhileRenaming(self):
+		self.loadFilesButton.setEnabled(False)
+		self.renameFilesButton.setEnabled(False)
 		
 	def _runRenamerThread(self):
 		
 	def _updateStateWhenFileRenamed(self, newFile):
+		self._files.popleft()
+		self.scrFileList.takeItem(0)
+		self.dstFileList.addItem(str(newFile))
 		
 	def _updateProgressBar(self, fileNumber):
+		progressPercent = int(fileNumber / self._filesCount * 100)
+		self.progressBar.setValue(progressPercent)
